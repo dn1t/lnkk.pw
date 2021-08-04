f4b7fbaa1859354b71298be7fe2d357d5771ac4d
thoratica <me@tica.fun>
2021-08-04 18:22:39 +0900

    lnkk.pw

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { json, urlencoded } from 'express';
+import rateLimit from 'express-rate-limit';
 import db from 'quick.db';
 import { join } from 'path';
 
+const expressip = require('express-ip');
+
 const app = express();
 
-if (!db.has('urls')) db.set('urls', []);
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  message: '한 IP에서는 URL을 5분당 100개까지만 단축할 수 있습니다.',
+});
+
+if (!db.has('urls')) db.set('urls', {});
 if (!db.has('notice')) db.set('notice', '긴 URL을 짧게 단축하세요!');
 
 app.set('view engine', 'ejs');
@@ -12,12 +21,77 @@ app.set('views', join(__dirname, './views'));
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use('/shorten', limiter);
+app.use(expressip().getIpInfoMiddleware);
 app.use(express.static(join(__dirname, './public')));
 
 app.get('/', (_req, res) => {
   res.render('index');
 });
 
-app.listen(3000);
+app.get('/shorten', (req, res) => {
+  if (req.query.url === undefined)
+    return res.status(400).send('단축할 URL이 제공되지 않았습니다.');
+
+  const url = (req.query.url ?? '').toString();
+  const useCustom = req.query.custom !== undefined;
+  const id = useCustom
+    ? (req.query.custom ?? '').toString()
+    : (Object.keys(db.get('urls')).length + 1).toString();
+
+  if (
+    [
+      'admin',
+      'tica',
+      'thoratica',
+      'hayang1027',
+      'hayanggames',
+      'dukhwa',
+      'shorten',
+      'lnkk',
+      'lnkkpw',
+    ].includes(id) ||
+    !/^[a-zA-Z0-9]+$/.test(id)
+  )
+    return res.status(400).send('허용되지 않는 커스텀 URL입니다.');
+
+  if (
+    !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(
+      url
+    )
+  )
+    return res.status(400).send('단축할 URL이 올바르지 않습니다.');
+
+  if (useCustom && (id.length < 4 || id.length > 12))
+    return res
+      .status(400)
+      .send('커스텀 URL은 4자 이상 12자 이하만 가능합니다.');
+
+  // @ts-expect-error
+  const ip = req.ipInfo.ip;
+
+  if (db.has(`urls.${id}`))
+    return res.status(400).send('이미 존재하는 커스텀 URL입니다.');
+
+  const data = {
+    url: url,
+    id: id,
+    ip: req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || ip,
+  };
+
+  db.set(`urls.${id}`, data);
+
+  res.json(data);
+});
+
+app.get('/:id', (req, res) => {
+  if (req.params.id === undefined) return res.redirect('/');
+  const url = db.get(`urls.${req.params.id.toString()}`)?.url;
+  if (url === undefined) return res.redirect('/');
+
+  res.redirect(url);
+});
+
+app.listen(8080);
 
 export default app;
--- src/public/index.js
+++ src/public/index.js
@@ -1,22 +1,115 @@
 const toggleAdvanced = (checked, isFirst) => {
+  const main = document.getElementsByTagName('main')[0];
+
   Array.from(document.getElementsByClassName('advanced')).forEach((el) => {
     if (checked) {
-      el.classList.remove('h-0', 'opacity-0');
-      el.classList.add('mt-2');
+      el.classList.replace('max-h-0', 'max-h-18');
+      el.classList.replace('opacity-0', 'mt-1');
+      main.classList.replace('max-w-sm', 'max-w-md');
     } else {
-      el.classList.remove('mt-2');
-      el.classList.add('h-0', 'opacity-0');
+      el.classList.replace('max-h-18', 'max-h-0');
+      el.classList.replace('mt-1', 'opacity-0');
+      main.classList.replace('max-w-md', 'max-w-sm');
     }
 
     if (isFirst)
-      setTimeout(() => el.classList.add('transition-all', 'duration-500'), 10);
+      setTimeout(() => {
+        el.classList.add('transition-all', 'duration-300');
+        main.classList.add('transition-all');
+      }, 10);
   });
 };
 
+const toggleModal = () => {
+  const modal = document.getElementById('modal');
+  const background = document.getElementById('background');
+
+  if (modal.classList.contains('opacity-100')) {
+    modal.classList.replace('opacity-100', 'opacity-0');
+    modal.classList.replace('translate-y-4', 'translate-y-0');
+    modal.classList.replace('sm:scale-95', 'sm:scale-100');
+    modal.classList.remove('sm:translate-y-0');
+
+    background.classList.replace('opacity-100', 'opacity-0');
+
+    setTimeout(
+      () => document.querySelector('div[role=dialog]').classList.add('hidden'),
+      300
+    );
+  } else {
+    document.querySelector('div[role=dialog]').classList.remove('hidden');
+    modal.classList.replace('opacity-0', 'opacity-100');
+    modal.classList.replace('translate-y-0', 'translate-y-4');
+    modal.classList.replace('sm:scale-100', 'sm:scale-95');
+    modal.classList.add('sm:translate-y-0');
+
+    background.classList.replace('opacity-0', 'opacity-100');
+  }
+};
+
+const copyUrl = () => {
+  navigator.clipboard
+    .writeText(document.getElementById('result').value)
+    .then(() => {
+      Toastify({
+        text: 'URL이 클립보드에 복사되었습니다.',
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'right',
+        backgroundColor: 'white',
+        className: 'copied shadow-md rounded-md',
+        stopOnFocus: true,
+      }).showToast();
+      toggleModal();
+    });
+};
+
 window.addEventListener('DOMContentLoaded', () => {
+  const alertError = (text) =>
+    Toastify({
+      text,
+      duration: 3000,
+      gravity: 'bottom',
+      position: 'right',
+      backgroundColor: 'white',
+      className: 'error shadow-md rounded-md',
+      stopOnFocus: true,
+    }).showToast();
+
   const custom = document.getElementById('custom');
+  const link = document.getElementById('link');
+
+  link.addEventListener('keyup', (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9]/gi, '');
+  });
 
   toggleAdvanced(custom.checked, true);
 
   custom.addEventListener('click', (e) => toggleAdvanced(e.target.checked));
+
+  document.getElementById('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    if (custom.checked && (link.length < 4 || link.length > 12))
+      return alertError('커스텀 URL은 4자 이상 12자 이하만 가능합니다.');
+
+    const res = await fetch(
+      `/shorten?url=${e.target[0].value}${
+        custom.checked ? `&custom=${e.target[1].value}` : ''
+      }`
+    );
+
+    if (!res.ok) {
+      if (res.status === 400) return alertError(await res.text());
+      else return notyf.error('알 수 없는 에러가 발생했습니다.');
+    }
+
+    e.target[0].value = '';
+    e.target[1].value = '';
+
+    document.getElementById('result').value = `https://lnkk.pw/${
+      (await res.json()).id
+    }`;
+    toggleModal();
+  });
 });
